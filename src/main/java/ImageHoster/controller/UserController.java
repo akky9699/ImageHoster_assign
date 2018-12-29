e74838f13400fd4fbcc3e0229c5121d739eb6d71
akky9699 <akshayakky8828@gmail.com>
2018-12-29 23:24:04 +0530

    validation added for password field to check strength(should contain atleast 1 alphabet, 1 number & 1 special character) of the password entered by the user at the time of registration.
    in the registerUser function  validation added for password field to check strength(should contain atleast 1 alphabet, 1 number & 1 special character) of the password entered by the user at the time of registration.

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -39,8 +39,15 @@ public class UserController {
 
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
+    //validation added for password field to check strength(should contain atleast 1 alphabet, 1 number & 1 special character) of the password entered by the user at the time of registration.
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
+    public String registerUser(User user,Model model) {
+        if(!(user.getPassword().matches("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{3,20})"))){
+            String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+            model.addAttribute("User",user);
+            model.addAttribute("passwordTypeError",error);
+            return "users/registration";
+        }
         userService.registerUser(user);
         return "redirect:/users/login";
     }
